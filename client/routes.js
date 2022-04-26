750b1bcf149c0f6209e6da32e80e872ba809f694
Daniel McNelis <dwm253@gmail.com>
2022-04-26 19:03:35 -0400

    reduced bg size

--- client/routes.js
+++ client/routes.js
@@ -40,29 +40,29 @@ const Routes = () => {
       <Route path="/leaderboards/:id" component={LeaderBoard} />
       <Route path="/banlists/:id" component={SingleBanList} />
       <Route path="/players/:id" component={PlayerProfile} />
-      <Route path="/may-2002---yugi-kaiba"><Redirect to="/formats/Yugi-Kaiba"/></Route>
-      <Route path="/jul-2002---critter"><Redirect to="/formats/Critter"/></Route>
-      <Route path="/apr-2003---android"><Redirect to="/formats/Android"/></Route>
-      <Route path="/aug-2003---yata"><Redirect to="/formats/Yata"/></Route>
-      <Route path="/aug-2004---chaos"><Redirect to="/formats/Chaos"/></Route>
-      <Route path="/oct-2004---warrior"><Redirect to="/formats/Warrior"/></Route>
-      <Route path="/apr-2005---goat"><Redirect to="/formats/Goat"/></Route>
-      <Route path="/oct-2005---reaper"><Redirect to="/formats/Reaper"/></Route>
-      <Route path="/apr-2006---chaos-return"><Redirect to="/formats/Chaos Return"/></Route>
-      <Route path="/oct-2006---stein-monarch"><Redirect to="/formats/Stein"/></Route>
-      <Route path="/mar-2007---troop-dup"><Redirect to="/formats/Troop Dup"/></Route>
-      <Route path="/jan-2008---perfect-circle"><Redirect to="/formats/Perfect Circle"/></Route>
-      <Route path="/sep-2008---teledad"><Redirect to="/formats/TeleDAD"/></Route>
-      <Route path="/mar-2009---synchro-cat"><Redirect to="/formats/Synchro Cat"/></Route>
-      <Route path="/mar-2010---edison"><Redirect to="/formats/Edison"/></Route>
-      <Route path="/july-2010---glad-beast"><Redirect to="/formats/Frog"/></Route>
-      <Route path="/oct-2011---tengu-plant"><Redirect to="/formats/Tengu Plant"/></Route>
-      <Route path="/dec-2012---wind-up"><Redirect to="/formats/Wind-Up"/></Route>
-      <Route path="/mar-2013---baby-ruler"><Redirect to="/formats/Baby Ruler"/></Route>
-      <Route path="/sep-2013---ravine-ruler"><Redirect to="/formats/Ravine Ruler"/></Route>
-      <Route path="/july-2014---hat"><Redirect to="/formats/HAT"/></Route>
-      <Route path="/july-2015---djinn-lock"><Redirect to="/formats/Nekroz"/></Route>
-      <Route path="/aug-2015---newgioh"><Redirect to="/formats/"/></Route>
+      <Route exact path="/may-2002---yugi-kaiba"><Redirect to="/formats/Yugi-Kaiba"/></Route>
+      <Route exact path="/jul-2002---critter"><Redirect to="/formats/Critter"/></Route>
+      <Route exact path="/apr-2003---android"><Redirect to="/formats/Android"/></Route>
+      <Route exact path="/aug-2003---yata"><Redirect to="/formats/Yata"/></Route>
+      <Route exact path="/aug-2004---chaos"><Redirect to="/formats/Chaos"/></Route>
+      <Route exact path="/oct-2004---warrior"><Redirect to="/formats/Warrior"/></Route>
+      <Route exact path="/apr-2005---goat"><Redirect to="/formats/Goat"/></Route>
+      <Route exact path="/oct-2005---reaper"><Redirect to="/formats/Reaper"/></Route>
+      <Route exact path="/apr-2006---chaos-return"><Redirect to="/formats/Chaos Return"/></Route>
+      <Route exact path="/oct-2006---stein-monarch"><Redirect to="/formats/Stein"/></Route>
+      <Route exact path="/mar-2007---troop-dup"><Redirect to="/formats/Troop Dup"/></Route>
+      <Route exact path="/jan-2008---perfect-circle"><Redirect to="/formats/Perfect Circle"/></Route>
+      <Route exact path="/sep-2008---teledad"><Redirect to="/formats/TeleDAD"/></Route>
+      <Route exact path="/mar-2009---synchro-cat"><Redirect to="/formats/Synchro Cat"/></Route>
+      <Route exact path="/mar-2010---edison"><Redirect to="/formats/Edison"/></Route>
+      <Route exact path="/july-2010---glad-beast"><Redirect to="/formats/Frog"/></Route>
+      <Route exact path="/oct-2011---tengu-plant"><Redirect to="/formats/Tengu Plant"/></Route>
+      <Route exact path="/dec-2012---wind-up"><Redirect to="/formats/Wind-Up"/></Route>
+      <Route exact path="/mar-2013---baby-ruler"><Redirect to="/formats/Baby Ruler"/></Route>
+      <Route exact path="/sep-2013---ravine-ruler"><Redirect to="/formats/Ravine Ruler"/></Route>
+      <Route exact path="/july-2014---hat"><Redirect to="/formats/HAT"/></Route>
+      <Route exact path="/july-2015---djinn-lock"><Redirect to="/formats/Nekroz"/></Route>
+      <Route exact path="/aug-2015---newgioh"><Redirect to="/formats/"/></Route>
       <Route component={NotFound} />
     </Switch>
   )
