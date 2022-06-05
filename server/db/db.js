f507f8b08ccb92d80b96efdc4bc25fc558fe7b51
Daniel McNelis <dwm253@gmail.com>
2022-06-04 22:56:05 -0400

    flc24 bracket

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const { pgPassword } = require('../../secrets.js')
 const pkg = require('../../package.json')
 // const fs = require('fs')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-const onAWS = true
+const onAWS = false
 const url = process.env.DATABASE_URL
   ? process.env.DATABASE_URL
   : `postgresql://danielmcnelis@localhost/${databaseName}`
