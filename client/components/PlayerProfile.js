f43ccc4c353376955cb0535d49b7c250ede3cfb1
Daniel McNelis <dwm253@gmail.com>
2022-06-06 03:57:51 -0400

    DLC02 coverage

--- client/components/PlayerProfile.js
+++ client/components/PlayerProfile.js
@@ -99,7 +99,7 @@ const PlayerProfile = (props = {}) => {
                                     <div className="player-profile-cell">
                                         <div><b>Name:</b> {player.realName || 'N/A'}</div>
                                     </div>       
-                                </td>f
+                                </td>
                             </tr>
                             <tr className="player-profile-info">
                                 <td>
