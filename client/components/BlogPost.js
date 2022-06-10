69a4b01a2a267dacfec5f2a655ec36cb448986e2
Daniel McNelis <dwm253@gmail.com>
2022-06-09 23:42:20 -0400

    adsense

--- client/components/BlogPost.js
+++ client/components/BlogPost.js
@@ -1,17 +1,17 @@
 
 import React from 'react'
 import parse from 'html-react-parser'
-import {Adsense} from '@ctrl/react-adsense';
+import Adsense from 'react-adsense';
 
 const BlogPost = (props) => {
   return (
     <div className="blogpost">
       <div className="blogpost-flexbox">
         <div className="post-content">{parse(props.content)}</div>
-        <Adsense
+        <Adsense.Google
           className="vertical-adsense"
-          client="ca-pub-7640562161899788"
-          slot="7259870550"
+          client="ca-pub-2048547741313745"
+          slot="8384346734"
           style={{ width: '100%', height: '75%' }}
           format=""
         />
