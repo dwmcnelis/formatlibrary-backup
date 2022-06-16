993e1f8671b0daaeb4e8a4082cddbab277dbfcda
Daniel McNelis <dwm253@gmail.com>
2022-06-15 22:55:50 -0400

    admin portal

--- server/index.js
+++ server/index.js
@@ -54,8 +54,8 @@ const createApp = () => {
 	app.use(morgan('dev'))
 
 	// body parsing middleware
-	app.use(express.json())
-	app.use(express.urlencoded({ extended: true }))
+	app.use(express.json({limit: '1mb'}))
+	app.use(express.urlencoded({ extended: true, limit: '1mb'}))
 
 	// compression middleware
 	app.use(compression())
