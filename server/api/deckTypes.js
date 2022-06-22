485969500dacfd6dd7f5b37ebed789195da06202
Daniel McNelis <dwm253@gmail.com>
2022-06-22 17:48:48 -0400

    admin portal

--- server/api/deckTypes.js
+++ server/api/deckTypes.js
@@ -1,8 +1,8 @@
 
 const router = require('express').Router()
 const {DeckThumb, DeckType} = require('../db/models')
-const {capitalize} = require('../../functions/utility')
-const {Op} = require('sequelize')
+const axios = require('axios')
+const fs = require('fs')
 
 module.exports = router
 
@@ -26,6 +26,51 @@ router.post('/create', async (req, res, next) => {
             rightCard: req.body.rightCardName,
             rightCardYpdId: req.body.rightCardYpdId,
         })
+        
+        if (!fs.existsSync(`./public/images/artworks/${deckThumb.leftCardYpdId}.jpg`)) {
+            try {
+                const {data} = await axios({
+                    method: 'GET',
+                    url: `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${deckThumb.leftCardYpdId}.jpg`,
+                    responseType: 'stream'
+                })
+    
+                data.pipe(fs.createWriteStream(`./public/images/artworks/${deckThumb.leftCardYpdId}.jpg`))
+                console.log(`saved ${deckThumb.leftCard} artwork to ${`./public/images/artworks/${deckThumb.leftCardYpdId}.jpg`}`)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        
+        if (!fs.existsSync(`./public/images/artworks/${deckThumb.centerCardYpdId}.jpg`)) {
+            try {
+                const {data} = await axios({
+                    method: 'GET',
+                    url: `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${deckThumb.centerCardYpdId}.jpg`,
+                    responseType: 'stream'
+                })
+    
+                data.pipe(fs.createWriteStream(`./public/images/artworks/${deckThumb.centerCardYpdId}.jpg`))
+                console.log(`saved ${deckThumb.centerCard} artwork to ${`./public/images/artworks/${deckThumb.centerCardYpdId}.jpg`}`)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        if (!fs.existsSync(`./public/images/artworks/${deckThumb.rightCardYpdId}.jpg`)) {
+            try {
+                const {data} = await axios({
+                    method: 'GET',
+                    url: `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${deckThumb.rightCardYpdId}.jpg`,
+                    responseType: 'stream'
+                })
+    
+                data.pipe(fs.createWriteStream(`./public/images/artworks/${deckThumb.rightCardYpdId}.jpg`))
+                console.log(`saved ${deckThumb.rightCard} artwork to ${`./public/images/artworks/${deckThumb.rightCardYpdId}.jpg`}`)
+            } catch (err) {
+                console.log(err)
+            }
+        }
 
         res.json(deckType)
     } catch (err) {
