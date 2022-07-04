8af3616c0edda45912c4cf187c3deec475720f12
Daniel McNelis <dwm253@gmail.com>
2022-07-04 14:14:40 -0400

    yrs49 and player urls

--- server/api/players.js
+++ server/api/players.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res, next) => {
   try {
     const player = await Player.findOne({
       where: {
-        tag: req.params.id.replaceAll('%2F', '/').replaceAll('%3F', '?').replaceAll('%23', '#'),
+        tag: req.params.id.replaceAll('%3F', '?').replaceAll('%23', '#'),
         blacklisted: false
       },
       attributes: { exclude: ['blacklisted', 'password', 'createdAt', 'updatedAt'] }
