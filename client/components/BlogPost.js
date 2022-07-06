7219b78e88a055ff6c802cbec4e11a50348ef371
Daniel McNelis <dwm253@gmail.com>
2022-07-06 18:25:22 -0400

    homepage

--- client/components/BlogPost.js
+++ client/components/BlogPost.js
@@ -7,27 +7,50 @@ const BlogPost = (props) => {
   return (
     <div className="blogpost">
       <div className="blogpost-flexbox">
-        <div className="post-content">{parse(props.content)}</div>
-          {
+        <div className="post-content">
+          {parse(props.content)}
+        </div>
+        {
             props.index === 0 ? (
-              <div className="vertical-ads-flexbox">
+              <div className="vertical-ads-flexbox" style={{height: "100%"}}>
+                <br/>
                 <a 
                   href="https://discord.com/invite/formatlibrary" 
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <div className="discord-link">
-                    <div className="discord-logo"/>
+                    <div className="discord-sidebar"/>
                     <p className="discord-desc-black">Join our Discord!</p>
                   </div>
                 </a>
-                <Adsense.Google
-                  className="vertical-adsense"
-                  client="ca-pub-2048547741313745"
-                  slot="8384346734"
-                  style={{ width: '100%', height: '50%' }}
-                  format=""
-                />
+                <br/>
+                <br/>
+                <a 
+                  href="https://www.buymeacoffee.com/danielmcnelis" 
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img 
+                    src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
+                    alt="Buy Me A Coffee" 
+                    style={{ height: "22%", width: "80%"}}
+                  />
+                </a>
+                <br/>
+                <br/>
+                <a 
+                  href="https://www.tcgplayer.com/search/yugioh/product?productLineName=yugioh&view=grid&utm_campaign=affiliate&utm_medium=FormatLibrary&utm_source=FormatLibrary" 
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img 
+                    src="/images/logos/TCGPlayer.png" 
+                    alt="TCG Player" 
+                    style={{height: "22%", width: "80%"}}
+                  />
+                  <p style={{color: "black", margin: "4px 0px"}}>Shop to support our site!</p>
+                </a>
               </div>
             ) : <Adsense.Google
               className="vertical-adsense"
@@ -37,7 +60,7 @@ const BlogPost = (props) => {
               format=""
             />
           }
-      </div>
+        </div>
       <div className="blog-divider"/>
     </div>
   )
--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,16 +4,30 @@ import {Link} from 'react-router-dom'
 //Footer
 const Footer = () => (
   <div className="footer">
-    <a 
-      href="https://discord.com/invite/formatlibrary" 
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div className="discord-link">
+    <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+      <a 
+        href="https://discord.com/invite/formatlibrary" 
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="discord-link">
           <div className="discord-logo"/>
           <p className="discord-desc">Join our Discord!</p>
-      </div>
-    </a>
+        </div>
+      </a>
+      <br/>
+      <a 
+        href="https://www.buymeacoffee.com/danielmcnelis" 
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img 
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
+          alt="Buy Me A Coffee" 
+          style={{height: "20%", width: "73%"}}
+        />
+      </a>
+    </div>
     <div>
         <div id="footer-menu">
           <Link to="/cards/">
@@ -31,9 +45,25 @@ const Footer = () => (
         </div>
         <p className="disclaimer">Format Library is a public resource for learning about the Yu-Gi-Oh! Trading Card Game and its history. Trademarked artwork and card text is used for informational purposes under U.S. fair use copyright policy. Not affiliated with 4K Media or Konami Digital Entertainment.</p>
     </div>
-    <Link to="/">
-      <div className="footer-logo"/>
-    </Link>
+    
+    <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+      <Link to="/">
+          <div className="footer-logo"/>
+      </Link>
+      <br/>
+      <a 
+        href="https://www.tcgplayer.com/search/yugioh/product?productLineName=yugioh&view=grid&utm_campaign=affiliate&utm_medium=FormatLibrary&utm_source=FormatLibrary" 
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img 
+          src="/images/logos/TCGPlayer.png" 
+          alt="TCG Player" 
+          style={{height: "20%", width: "73%"}}
+        />
+        <p style={{color: "white", margin: "4px 0px"}}>Shop to support our site!</p>
+      </a>
+    </div>
   </div>
 )
 
