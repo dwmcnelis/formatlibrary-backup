5d0ae721da93d655cc4cb90974d9358c77e9f714
Daniel McNelis <dwm253@gmail.com>
2022-07-06 23:34:04 -0400

    homepage

--- client/components/BlogPost.js
+++ client/components/BlogPost.js
@@ -13,20 +13,20 @@ const BlogPost = (props) => {
         {
             props.index === 0 ? (
               <div className="vertical-ads-flexbox" style={{height: "100%"}}>
-                <br/>
                 <a 
                   href="https://discord.com/invite/formatlibrary" 
                   target="_blank"
                   rel="noopener noreferrer"
+                  style={{border: "8px 0px"}}
                 >
                   <div className="discord-link">
                     <div className="discord-sidebar"/>
                     <p className="ad-desc">Join our Discord!</p>
                   </div>
                 </a>
-                <br/>
                 <a 
                   href="https://twitter.com/formatlibrary"
+                  style={{border: "8px 0px"}}
                 >
                   <img 
                     src="/images/logos/Twitter.png" 
@@ -35,11 +35,11 @@ const BlogPost = (props) => {
                   />
                   <p className="ad-desc">Follow us for Event Announcements!</p>
                 </a>
-                <br/>
                 <a 
                   href="https://www.buymeacoffee.com/danielmcnelis" 
                   target="_blank"
                   rel="noreferrer"
+                  style={{border: "8px 0px"}}
                 >
                   <img 
                     src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
@@ -47,11 +47,11 @@ const BlogPost = (props) => {
                     style={{ height: "22%", width: "80%"}}
                   />
                 </a>
-                <br/>
                 <a 
                   href="https://www.tcgplayer.com/search/yugioh/product?productLineName=yugioh&view=grid&utm_campaign=affiliate&utm_medium=FormatLibrary&utm_source=FormatLibrary" 
                   target="_blank"
                   rel="noreferrer"
+                  style={{border: "8px 0px"}}
                 >
                   <img 
                     src="/images/logos/TCGPlayer.png" 
@@ -65,7 +65,7 @@ const BlogPost = (props) => {
               className="vertical-adsense"
               client="ca-pub-2048547741313745"
               slot="8384346734"
-              style={{ textAlign: 'center', width: '100%', height: '75%' }}
+              style={{ alignItems: 'center', textAlign: 'center', width: '100%', height: '75%' }}
               format=""
             />
           }
