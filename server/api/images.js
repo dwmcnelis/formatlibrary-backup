e526b018d947d64d485851d848346af7fb1e746f
Daniel McNelis <dwm253@gmail.com>
2022-07-18 18:20:26 -0400

    create images

--- server/api/images.js
+++ server/api/images.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 
 module.exports = router
 
-router.get('/create', async (req, res, next) => {
+router.post('/create', async (req, res, next) => {
     try {
         const buffer = req.body.image.replace(/^data:image\/png;base64,/, '')
         fs.writeFileSync(`./public/images/${req.body.folder}/${req.body.fileName}`, buffer, 'base64')
