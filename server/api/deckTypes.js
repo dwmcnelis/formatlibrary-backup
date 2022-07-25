aa4bfec0c2cededea5a7a7d2a447ccdeec85e556
Daniel McNelis <dwm253@gmail.com>
2022-07-25 03:32:46 -0400

    debug

--- server/api/deckTypes.js
+++ server/api/deckTypes.js
@@ -18,9 +18,9 @@ router.get('/:id', async (req, res, next) => {
         const freqs = decks.reduce((acc, curr) => (acc[curr.formatName] ? acc[curr.formatName]++ : acc[curr.formatName] = 1, acc), {})
         const sortedFreqs = Object.entries(freqs).sort((a, b) => b[1] - a[1])
         const topFormat = sortedFreqs[0][0]
-        const count = sortedFreqs[req.headers.format]
         const format = await Format.findOne({ where: { name: {[Op.iLike]: req.headers.format || topFormat } }})
         const showExtra = format.date >= '2008-08-05'
+        const count = sortedFreqs[format.name]
         console.log('format.name', format.name)
         console.log('count', count)
         const total = await Deck.count({ where: { formatName: {[Op.iLike]: format.name} }})
