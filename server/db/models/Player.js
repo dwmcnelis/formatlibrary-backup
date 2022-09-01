5aafe93d277f752692ca63b190f4ce669a98f95e
Daniel McNelis <dwm253@gmail.com>
2022-09-01 00:18:19 -0400

    player model

--- server/db/models/Player.js
+++ server/db/models/Player.js
@@ -9,13 +9,34 @@ const Player = db.define('players', {
     email: {
         type: Sequelize.STRING
     },
-    discordId: {
+    avatar: {
+        type: Sequelize.STRING,
+    },
+    discordName: {
         type: Sequelize.STRING
     },
     discriminator: {
         type: Sequelize.STRING
     },
+    discordId: {
+        type: Sequelize.STRING
+    },
+    discordPfp: {
+        type: Sequelize.STRING
+    },
+    googleName: {
+        type: Sequelize.STRING
+    },
+    googleId: {
+        type: Sequelize.STRING
+    },
+    googlePfp: {
+        type: Sequelize.STRING
+    },
     duelingBook: {
+        type: Sequelize.STRING
+    },
+    duelingBookPfp: {
         type: Sequelize.STRING
     },
     firstName: {
@@ -34,10 +55,42 @@ const Player = db.define('players', {
     hidden: {
         type: Sequelize.BOOLEAN,
         defaultValue: false
-    },
-    avatar: {
-        type: Sequelize.STRING
     }
 })
+
+// Player.findByDiscordId = (id) => Player.findOne({ where: { discordId: id }})
+
+// Player.resolveDiscord = async (user) => {
+//     let existingPlayer = await Player.findOne({ 
+//         where: { 
+//             discordId: user.id
+//         }
+//     })
+
+//     if (existingPlayer) {
+
+//         await existingPlayer.update({
+//             avatar: user.avatar
+//         })
+
+//         return existingPlayer
+
+//     } else {
+//         existingPlayer = await Player.findOne({ 
+//             where: { 
+//                 email: user.email    
+//             }
+//         })
+
+//         if (existingPlayer) {
+
+//             return existingPlayer
+//         }
+//     }
+
+//    return await Player.create({})
+// }
+
+// Player.prototype.hide = () => this.update({ hidden: true })
 
 module.exports = Player
